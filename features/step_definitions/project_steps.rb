e939618b87499e702468d61d21f56cad88e52201
bill auger <mr.j.spam.me@gmail.com>
2014-10-26 23:46:36 -0400

    remove tests
    * these tests have been slightly modified and moved to the 'manage-project' branch
          to avoid conflicts with the newer, more generalized web_steps

--- features/step_definitions/project_steps.rb
+++
@@ -1,9 +0,0 @@
-
-Then /^There should (.*)\s*be a project avatar image$/ do |should|
-  avatar_xpath = "//img[contains(@src, \"githubusercontent\")]"
-  if should.eql? 'not '
-    page.should_not have_xpath avatar_xpath
-  else
-    page.should have_xpath avatar_xpath
-  end
-end
