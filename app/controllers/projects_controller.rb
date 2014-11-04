7f2c5de4ea1b4109ff747d9a7f3a6eeb2759fbd3
bill auger <mr.j.spam.me@gmail.com>
2014-11-04 08:30:29 +0000

    merge the remainder of project-management branch

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,10 @@
 require 'net/http'
 
 class ProjectsController < ApplicationController
+  include ProjectsHelper
 
-  before_filter :load_project, only: [:show, :edit, :update, :decide_tip_amounts,
-                                      :tips, :deposits]
+  before_filter :load_project, only: [:show, :edit, :update, :decide_tip_amounts]
+  before_filter :redirect_to_pretty_url, only: [:show, :edit, :decide_tip_amounts]
 
   def index
     @projects = Project.order(projects_order).page(params[:page]).per(30)
@@ -22,43 +23,11 @@ class ProjectsController < ApplicationController
     render :index
   end
 
-  # Redirect to pretty url for html format
-  include ProjectsHelper
-  before_filter only: [:show, :edit, :decide_tip_amounts] do |controller|
-    return unless request.get?
-
-    if params[:id].present?
-      begin
-        respond_to do |format|
-          case controller.action_name
-          when 'show'
-            path = pretty_project_path(@project)
-          when 'edit'
-            path = pretty_project_edit_path(@project)
-          when 'decide_tip_amounts'
-            path = pretty_project_decide_tip_amounts_path(@project)
-          end
-          format.html { redirect_to path }
-        end
-      rescue ActionController::UnknownFormat
-      end
-    end
-  end
-
   def show
-    if BLACKLIST.include?(@project.github_url)
-      return render :blacklisted
-    end
+    render :blacklisted and return if BLACKLIST.include? @project.github_url
 
-    if @project.bitcoin_address.nil?
-      uri = URI("https://blockchain.info/merchant/#{CONFIG["blockchain_info"]["guid"]}/new_address")
-      params = { password: CONFIG["blockchain_info"]["password"], label:"#{@project.full_name}@tip4commit" }
-      uri.query = URI.encode_www_form(params)
-      res = Net::HTTP.get_response(uri)
-      if res.is_a?(Net::HTTPSuccess) && (bitcoin_address = JSON.parse(res.body)["address"])
-        @project.update_attribute :bitcoin_address, bitcoin_address
-      end
-    end
+    @project.update_bitcoin_address if @project.bitcoin_address.nil?
+
     @project_tips = @project.tips.with_address
     @recent_tips  = @project_tips.with_address.order(created_at: :desc).first(5)
   end
@@ -127,5 +96,24 @@ class ProjectsController < ApplicationController
       'repository'  => {full_name: :asc, available_amount_cache: :desc, watchers_count: :desc},
       'description' => {description: :asc, available_amount_cache: :desc, watchers_count: :desc, full_name: :asc}
     }.[](params[:order] || 'balance')
+  end
+
+  def redirect_to_pretty_url
+    return unless request.get? && params[:id].present?
+
+    begin
+      respond_to do |format|
+        case action_name
+        when 'show'
+          path = pretty_project_path(@project)
+        when 'edit'
+          path = pretty_project_edit_path(@project)
+        when 'decide_tip_amounts'
+          path = pretty_project_decide_tip_amounts_path(@project)
+        end
+        format.html { redirect_to path }
+      end
+    rescue ActionController::UnknownFormat
+    end
   end
 end
