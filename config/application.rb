dab832f25fd5fd802ed09e60c2314dfd020c8afa
Aleksandr Zykov <alexandrz@gmail.com>
2017-11-12 22:17:17 +0100

    set active job adapter to sidekiq + fixed rubocop warnings

--- config/application.rb
+++ config/application.rb
@@ -7,26 +7,26 @@ require 'rails/all'
 Bundler.require(:default, Rails.env)
 
 # load config.yaml preprocessed
-CONFIG ||= YAML::load(ERB.new(File.read("config/config.yml")).result)
+CONFIG ||= YAML.safe_load(ERB.new(File.read('config/config.yml')).result)
 
 module T4c
   class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
 
-  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-  # config.time_zone = 'Central Time (US & Canada)'
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
 
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-  # config.i18n.default_locale = :de
-
-  config.autoload_paths += %W(#{config.root}/lib)
-  config.assets.initialize_on_precompile = true
-  config.available_locales = %w(en fr nl ru pl hr de ro)
+    config.autoload_paths += %W[#{config.root}/lib]
+    config.assets.initialize_on_precompile = true
+    config.available_locales = %w[en fr nl ru pl hr de ro]
+    config.active_job.queue_adapter = :sidekiq
   end
 end
 
