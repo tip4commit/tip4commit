ff7ff2ec81f381c8305a3654f19b603a167f2c40
Aleksandr Zykov <alexandrz@gmail.com>
2020-12-06 18:33:49 +0100

    refactored omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,31 +2,35 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    before_action :load_omniauth_info, only: :github
+    before_action :load_omniauth_info,
+                  :load_user_from_omniauth_info,
+                  :update_user_from_omniauth_info, only: :github
 
     def github
-      @user = User.find_by(nickname: @omniauth_info.nickname) ||
-              User.find_by(email: @omniauth_info.email)
-
-      if @user.present?
-        if @omniauth_info.email.present? && @user.email != @omniauth_info.email
-          # update email if it has been changed
-          @user.update email: @omniauth_info.email
-        end
-      elsif @omniauth_info.email.present? # user not found
-        @user = User.create_with_omniauth!(@omniauth_info)
-      else
-        set_flash_message(:error, :failure, kind: 'GitHub', reason: I18n.t('devise.errors.primary_email'))
-        redirect_to new_user_session_path and return
-      end
-
-      @user.update(@omniauth_info.slice(:name, :image).as_json)
-
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
     end
 
     private
+
+    def update_user_from_omniauth_info
+      update_hash = @omniauth_info.slice(:name, :image).as_json
+      update_hash[:email] = @omniauth_info.email if @omniauth_info.email.present?
+
+      @user.update(update_hash)
+    end
+
+    def load_user_from_omniauth_info
+      @user = User.find_by(nickname: @omniauth_info.nickname) ||
+              User.find_by(email: @omniauth_info.email)
+      return if @user
+
+      @user = User.create_with_omniauth!(@omniauth_info) if @omniauth_info.email.present?
+      return if @user
+
+      set_flash_message(:error, :failure, kind: 'GitHub', reason: I18n.t('devise.errors.primary_email'))
+      redirect_to new_user_session_path
+    end
 
     def load_omniauth_info
       @omniauth_info = request.env['omniauth.auth']['info']
